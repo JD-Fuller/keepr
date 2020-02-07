5ec00656b4bcc86f233552a9a0466a13453f799d
JD-Fuller <jdfuller@live.com>
2020-02-07 14:25:04 -0700

    auth and gearhost settings config'd

--- wwwsrc/src/authConfig.js
+++ wwwsrc/src/authConfig.js
@@ -1,3 +1,3 @@
-export const domain = "";
-export const audience = "";
-export const clientId = "";
+export const domain = "dev-full84r.auth0.com";
+export const audience = "https://dev-full84er-api.com";
+export const clientId = "UG2wZ7oNXZzM0yN1X25b1p5jItH7PH0L";
